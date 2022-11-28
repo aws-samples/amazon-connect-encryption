e7465afe644700cfc8396b61a1258006826e160d
Ed Valdez <evaldezg@amazon.com>
2022-11-28 13:46:45 -0500

    Updated DecryptFn to validate encryption context as recommended

--- src/stack-core/lambdas/decrypt/index.ts
+++ src/stack-core/lambdas/decrypt/index.ts
@@ -10,12 +10,21 @@ export async function handler(event: any, context: any) {
 			const encrypted = event.Details?.Parameters?.encrypted || '';
 			console.log(`DecryptFn: encrypted = [${encrypted}]`);
 			if(!encrypted) throw new Error(`Parameter is null or blank: [encrypted]`);
+
+			const CONTEXT = typeof process.env.ENCRYPTION_CONTEXT === 'string'
+				? JSON.parse(process.env.ENCRYPTION_CONTEXT) : undefined;
 						
 			// Set up for encryption SDK
 			const keyIds = [ process.env.WRAPPER_KEY! ];
 			const keyRing = new KmsKeyringNode({ keyIds });
 			const { decrypt } = buildClient(CommitmentPolicy.REQUIRE_ENCRYPT_REQUIRE_DECRYPT);			
-			const { plaintext } = await decrypt(keyRing, Buffer.from(encrypted, 'base64'));
+			const { plaintext, messageHeader } = await decrypt(keyRing, Buffer.from(encrypted, 'base64'));
+			const { encryptionContext } = messageHeader;
+
+			Object.entries(CONTEXT).forEach(([key, value]) => {
+				if (encryptionContext[key] !== value) throw new Error('Encryption Context does not match expected values')
+			});
+
 			resp = { Status: 'OK', Data: plaintext.toString() };
 
 		} catch(err: any) {
